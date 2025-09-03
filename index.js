b44e964b0553899a21534cc0781b4560ab87ae2e
Mitchell Bolton <admin@drbrendanlopez.com>
2025-09-03 16:39:30 -0700

    Disabled nav buttons and cleaned up cards.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-document.getElementById('nav-button').addEventListener('click', function(event) {
-    event.preventDefault();
-    console.log("test");
+document.querySelectorAll('.nav-link').forEach(button => {
+    button.addEventListener('click', function(event) {
+        event.preventDefault();
+        alert("Nav bar is not enabled for this demo. I just left it here to make the page look nicer.");
+    });
 });
+
