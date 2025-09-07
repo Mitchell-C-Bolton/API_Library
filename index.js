567349e2389214a8ccc20f41d5599d8af3cddfe7
Mitchell Bolton <mitchell-bolton@outlook.com>
2025-09-07 12:25:49 -0700

    Added my first working API for weather.

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+// DOM elements
+const seattleTempButton = document.getElementById("seattleTempButton");
+let seattleTemp = document.getElementById("seattleTemp");
+
+// API Links
+weatherURL = "https://api.open-meteo.com/v1/forecast?latitude=47.6062&longitude=-122.3321&current=temperature_2m&timezone=Pacific%2FAuckland&forecast_days=1&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch";
+
+
+// Fetch data Function
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return null;
+  }
+}
+
+//Update card functions
+async function updateWeather(url) {
+    seattleTemp.textContent = "Loadaing...";
+    const weatherData = await fetchData(url);
+    seattleTemp.textContent = "Temperature: " + weatherData.current.temperature_2m + 'F';
+}
+
+// Navbar message
 document.querySelectorAll('.nav-link').forEach(button => {
     button.addEventListener('click', function(event) {
         event.preventDefault();
@@ -5,3 +36,11 @@ document.querySelectorAll('.nav-link').forEach(button => {
     });
 });
 
+//Update Card Buttons
+seattleTempButton.addEventListener("click", () => {
+    updateWeather(weatherURL);
+    console.log('weather updated');
+});
+
+//Initialize
+updateWeather(weatherURL);
