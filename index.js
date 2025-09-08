d858aee5009141d1554a14a79472af6bf1b6d3db
Mitchell Bolton <mitchell-bolton@outlook.com>
2025-09-08 07:52:08 -0700

    Added two more API connections.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // DOM elements
 const seattleTempButton = document.getElementById("seattleTempButton");
+const catFactsButton = document.getElementById("catFactsButton");
+const dogImageButton = document.getElementById("dogImageButton");
 let seattleTemp = document.getElementById("seattleTemp");
+let catFacts = document.getElementById("catFacts");
+let dogImage = document.getElementById("dogImage");
 
 // API Links
 weatherURL = "https://api.open-meteo.com/v1/forecast?latitude=47.6062&longitude=-122.3321&current=temperature_2m&timezone=Pacific%2FAuckland&forecast_days=1&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch";
-
+catFactsURL = "https://meowfacts.herokuapp.com/";
+dogImageURL = "https://random.dog/woof.json";
 
 // Fetch data Function
 async function fetchData(url) {
@@ -22,11 +27,51 @@ async function fetchData(url) {
 }
 
 //Update card functions
-async function updateWeather(url) {
+async function updateWeather(url) { // Weather
     seattleTemp.textContent = "Loadaing...";
     const weatherData = await fetchData(url);
     seattleTemp.textContent = "Temperature: " + weatherData.current.temperature_2m + 'F';
 }
+
+async function updateCatFacts(url) { // Cat Fact
+    catFacts.textContent = "Loading...";
+    const catData = await fetchData(url);
+    catFacts.textContent = "Cat fact: " + catData.data;
+}
+
+async function updateDogImage(url) { // Dog Image
+    dogImage.textContent = "Loading...";
+    const dogImageData = await fetchData(url);
+
+    // only allow "images"
+    const validExtensions = [".jpg", ".jpeg", ".png", ".gif"];
+    const fileUrl = dogImageData.url;
+
+    if (validExtensions.some(ext => fileUrl.toLowerCase().endsWith(ext))) {
+        dogImage.src = fileUrl;
+        dogImage.alt = "A random dog";
+    } else {
+        // retry if not an image
+        console.log("Non-image returned, retrying...");
+        updateDogImage(url);
+    }
+}
+
+//Update Card Buttons
+seattleTempButton.addEventListener("click", () => { // Weather
+    updateWeather(weatherURL);
+    console.log('weather updated');
+});
+
+catFactsButton.addEventListener("click", () => { // Cat Fact
+    updateCatFacts(catFactsURL);
+    console.log('cat fact updated');
+});
+
+dogImageButton.addEventListener("click", () => { // Dog Image
+    updateDogImage(dogImageURL);
+    console.log('dog image updated');
+});
 
 // Navbar message
 document.querySelectorAll('.nav-link').forEach(button => {
@@ -36,11 +81,7 @@ document.querySelectorAll('.nav-link').forEach(button => {
     });
 });
 
-//Update Card Buttons
-seattleTempButton.addEventListener("click", () => {
-    updateWeather(weatherURL);
-    console.log('weather updated');
-});
-
 //Initialize
 updateWeather(weatherURL);
+updateCatFacts(catFactsURL);
+updateDogImage(dogImageURL);
